ad55e2225df927f1e8e22825a155cd805f4d872f
abherthel86 <abherthel@gmail.com>
2020-06-13 11:11:29 +0200

    test for 1B-2B 0-20 all sizes

--- script.sh
+++ script.sh
@@ -21,14 +21,14 @@
 #done
 
 dirInstancias='Instances/grubhub'
-output='testservermetrics1A2A0-30all.txt'
+output='testservermetrics1B2B0-20all.txt'
 #runs=10
 
 cat /dev/null > $output
 
-for i in 1A 2A
+for i in 1B 2B
 do
-	for j in 0 10 20 30
+	for j in 0 10 20
 	do
 		for arquivo in `ls ${dirInstancias}` #lista todos os arquivos na pasta que contem as instancias que vc quer rodar
 		do
