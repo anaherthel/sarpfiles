d9f477b2b5b08e1393ce924b193805c2fcb3b7eb
abherthel86 <abherthel@gmail.com>
2020-11-18 17:39:38 +0100

    ready for bundle test

--- script.sh
+++ script.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
 
+#dirInstancias='Instances/grubhub'
+#output='nGHSscaled150.txt'
+##runs=10
+
+#cat /dev/null > $output
+
+#for i in 1A 2A 1B 2B
+##for i in 1A
+#do
+#	for j in 10 20 30 40
+#	do
+#		for arquivo in `ls ${dirInstancias}` #lista todos os arquivos na pasta que contem as instancias que vc quer rodar
+#		do
+#			echo "Solving: " ${arquivo} ":" >> $output #output pra saber que instancia ta sendo rodada
+#			echo "Scenario: " $i >> $output
+#			echo "ParcelP: " $j >> $output
+#			./exeSARP ${dirInstancias}/${arquivo} $i $j node >> $output
+#		done
+#	done
+#done
+
 dirInstancias='Instances/grubhub'
-output='nGHSscaled150.txt'
+output='bGHSscaled150.txt'
 #runs=10
 
 cat /dev/null > $output
 
-for i in 1A 2A 1B 2B
-#for i in 1A
+#for i in 1A 2A 1B 2B
+for i in 1A
 do
 	for j in 10 20 30 40
 	do
@@ -16,11 +37,10 @@ do
 			echo "Solving: " ${arquivo} ":" >> $output #output pra saber que instancia ta sendo rodada
 			echo "Scenario: " $i >> $output
 			echo "ParcelP: " $j >> $output
-			./exeSARP ${dirInstancias}/${arquivo} $i $j node >> $output
+			./exeSARP ${dirInstancias}/${arquivo} $i $j bundle >> $output
 		done
 	done
 done
-
 
 #dirInstancias='Instances/csarp'
 #output='btestCustom.txt'
