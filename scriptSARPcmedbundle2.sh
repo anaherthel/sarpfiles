621483d984928d3fd3a870401882ed7c68d3eebd
abherthel86 <abherthel@gmail.com>
2024-03-12 14:54:07 +0100

    Ready for bundle tests

---
+++ scriptSARPcmedbundle2.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+dirInstancias='Instances_M2/csarp'
+output='bundlecsarp2.txt'
+#runs=10
+
+cat /dev/null > $output
+
+#for i in 1A
+#for i in 1A
+#do
+#	for j in 10 20 30 40
+#	do
+		for arquivo in `ls ${dirInstancias}` #lista todos os arquivos na pasta que contem as instancias que vc quer rodar
+		do
+#			echo "Solving: " ${arquivo} ":" >> $output #output pra saber que instancia ta sendo rodada
+#			echo "Scenario: 1A bundle "  >> $output
+			./exeSARP ${dirInstancias}/${arquivo} 1A bundle >> $output
+		done
+#	done
+#done
+
+
+
+
+
+
+
